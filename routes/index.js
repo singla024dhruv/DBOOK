7b2da094e43339030694e5dddb125ff4d83cc4cc
DHRUV <dhruvsingla024@gmail.com>
2023-08-21 18:55:38 +0530

    installed ejs

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,19 @@ const router=express.Router();
 const homeController=require('../controllers/home_controller');
 console.log('router is loaded');
 router.get('/',homeController.home);
-router.get('/try',homeController.try);
+//router.get('/try',homeController.try);
+
+//for any futhur routes access from here
+
+
+
+//router.use('/routername',require('./routes name))
+
+
+
+
+router.use('/users',require('./users'));
+
+
+
 module.exports = router;
