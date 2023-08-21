7c87981a6ddb948511b1c2c36781da2ab0ed63ae
DHRUV <dhruvsingla024@gmail.com>
2023-08-21 23:16:38 +0530

    some ejs file is added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,9 @@
 module.exports.home=function(req,res)
 {
-    return res.end('<h1>Express is up for DBOOK</h1>');
+   // return res.end('<h1>Express is up for DBOOK</h1>');
+   return res.render('home',{
+    title: "Home"
+   });
 }
 //module.exports.actionname= function(req,res){
 module.exports.try=function(req,res)
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,7 @@
 module.exports.profile=function(req,res)
 {
-    return res.end('<h1> users profile</h1>');
+    //return res.end('<h1> users profile</h1>');
+    return res.render('users_profile',{
+        title: "user_profile",
+    });
 }
