16c76d6526706b32bc6692f2c722b11f1ac8dc9f
DHRUV <dhruvsingla024@gmail.com>
2023-08-21 12:20:51 +0530

    add a controller and router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,9 @@
+module.exports.home=function(req,res)
+{
+    return res.end('<h1>Express is up for DBOOK</h1>');
+}
+//module.exports.actionname= function(req,res){
+module.exports.try=function(req,res)
+{
+    return res.end('<h1> another try</h1>');
+}
