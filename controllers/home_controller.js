7b97da33ae7b6e7813ab81ba55cc45328869edde
DHRUV <dhruvsingla024@gmail.com>
2023-09-19 17:05:43 +0530

    display of post with user name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,41 @@
-module.exports.home=function(req,res)
-{
-    console.log(req.cookies);
+const Post =require('../models/post');
+// module.exports.home=function(req,res)
+// {
+    //console.log(req.cookies);
     //send as a response
-    res.cookie('user_id',25);
+    //res.cookie('user_id',25);
    // return res.end('<h1>Express is up for DBOOK</h1>');
-   return res.render('home',{
-    title: "Home"
-   });
+//    Post.find({},function(err,posts){
+    
+//    return res.render('home',{
+//     title: "DBOOK | Home",
+//     posts:posts
+//    });
+// });
+// }
+ module.exports.home = async function(req, res) {
+//     try{const posts = await Post.find({});
+    
+//     return res.render('home', {
+//       title: "DBOOK | Home",
+//       posts: posts
+//     });
+//   }
+//   catch(err)
+//   {
+//     console.log('error in finding the post');
+//   }
+  // const post = await Post.
+  // findOne({ title: 'Casino Royale' }).
+  // populate('user').
+  // exec();
+  const posts = await Post.find({}).populate('user').exec();
+  res.render('home', {
+    title: "DBOOK| Home",
+    posts: posts
+  });
+  
+
 }
 //module.exports.actionname= function(req,res){
 
