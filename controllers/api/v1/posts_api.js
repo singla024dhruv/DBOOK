9191938dfe6b4d5b6a20dfb25b48a257a099e14e
DHRUV <dhruvsingla024@gmail.com>
2023-10-09 19:25:44 +0530

    such remove passport showing in API

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,10 +1,12 @@
 const Post =require('../../../models/post');
+const Comment=require('../../../models/comment');
 module.exports.index=async function(req,res){
     const posts = await Post.find({})
     .sort('-createdAt')
-    .populate('user').populate({
+    .populate({path: 'user',select:'-password'}).populate({
       path: 'comments',populate: {
-        path: 'user'
+        path: 'user',
+        select:'-password'
       }
     }
     ).exec();
@@ -12,4 +14,28 @@ module.exports.index=async function(req,res){
         message:"Lists of posts",
         posts:posts
     })
+}
+module.exports.destroy = async function(req, res) {
+  try{
+  let post = await Post.findById(req.params.id);
+  //if (post.user == req.user.id) {
+    await post.deleteOne({post:req.params.id});
+    
+    await Comment.deleteMany({ post: req.params.id });
+    
+   // }
+   // req.flash('success','post is deleted');
+    return res.json(200,{
+      message:"post and associated comments deleted"
+    });
+    }
+    catch(err){
+      console.log('error',err);
+      return res.json(500,{
+        message:"Internal server error"
+      });
+ // } else {
+   // res.redirect('back');
+  //}
+}
 }
